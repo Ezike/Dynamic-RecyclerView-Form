abbdfd201fddb7a8516445a4543e96e07a8f5c2b
Tobenna Ezike <Tobeydaniels@gmail.com>
2019-04-26 13:51:23 +0100

    Initial commit

---
+++ app/src/main/java/ezike/tobenna/petform/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package ezike.tobenna.petform.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ezike.tobenna.petform.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
